e57de986a27e86b450ddeb423eeca4a44849cb15
Marcell Dechant <marcell.dechant@neuefische.de>
2023-06-26 12:06:19 +0200

    Add backend module

---
+++ backend/src/test/java/de/neuefische/backendstudentdb/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.backendstudentdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
